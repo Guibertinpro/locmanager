51f0e17dcef838fb76448cf39a77795c90d80f1d
Guillaume BERTIN <80512116+Guibertinpro@users.noreply.github.com>
2023-05-18 22:58:59 +0200

    Change home access

--- assets/admin.js
+++ assets/admin.js
@@ -3,27 +3,4 @@ import './bootstrap';
 import 'popper.js';
 import 'fullcalendar';
 
-$(function () {
-
-  // Set field Arrhes to 30% of price per default
-  const priceField = $("#Reservation_price");
-  const arrhesField = $("#Reservation_arrhes");
-  const leftToPayField = $("#Reservation_leftToPay");
-  let arrhes = 0;
-  let leftToPay = 0;
-  priceField.on('change', () => {
-    const price = priceField.val();
-    arrhes = (price * 0.3);
-    arrhesField.val(arrhes);
-    leftToPay = (price - arrhes);
-    leftToPayField.val(leftToPay);
-  });
-
-  // Calculate the solde field value when arrhes value change.
-  arrhesField.on('change', () => {
-    const price = priceField.val();
-    arrhes = arrhesField.val();
-    leftToPay = (price - arrhes);
-    leftToPayField.val(leftToPay);
-  });
-});
+const $ = require('jquery');
